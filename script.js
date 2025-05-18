cef1c9778f69d9b3c01b6b517acd8fda7992713b
Davit kvarelashvili <kvarelashvili18@gmail.com>
2025-05-18 22:59:06 +0400

    improvement
    js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// data 18.05.2025
-let firstName = "data";
-let lastName = "kvarelashvili";
-let Email = "kvarelashvili18@gmail.com";
+// data
+// 18.05.2025
+
+const firstName = "data";
+const lastName = "kvarelashvili";
+const Email = "kvarelashvili18@gmail.com";
 let phoneNumber = "+995 598101110";
 let age = "25";
-let hoby = "skydiving";
+let hobby = "skydiving";
 
 const isStudent = true;
 
-console.log(firstName);
-console.log("Davit");
-console.log(lastName);
-console.log(Email);
-console.log(phoneNumber);
-console.log(age);
-console.log(hoby);
+console.log("firstName:" + firstName);
+console.log("lastName:" + lastName);
+console.log("Email:" + Email);
+console.log("phoneNumber:" + phoneNumber);
+console.log("age:" + age);
+console.log("hobby:" + hobby);
+console.log("isStudent:" + isStudent);
